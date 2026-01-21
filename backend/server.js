ec6cf5c331ce65bc8388cc3114747e24a92bcc89
Erkhtuguldur <erkhtugulduriderbat@gmail.com>
2026-01-20 18:13:48 -0800

    Use environment variable for PORT

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import { timeStamp } from "console";
 dotenv.config();
 
 const app=express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const httpServer=createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
